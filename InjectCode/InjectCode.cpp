d5f3ad561c3a6c7ccc581a2d6cd581410814694a
vektorprime <vektorhalili@gmail.com>
2024-02-27 08:00:46 -0600

    Modified code to reserve least privilege required. Added comments to explain the code.

--- InjectCode/InjectCode.cpp
+++ InjectCode/InjectCode.cpp
@@ -15,18 +15,33 @@ int main()
 
 	DWORD pid = std::stoi(pid_q);
 
-	HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+	HANDLE processHandle = OpenProcess(
+		PROCESS_QUERY_INFORMATION |		 // For Alloc
+		PROCESS_CREATE_THREAD |			 // For CreateRemoteThread
+		PROCESS_VM_OPERATION |			 // For VirtualAllocEx
+		PROCESS_VM_WRITE,				 // For WriteProcessMemory
+		FALSE, 
+		pid
+	);
 
 
 	if (processHandle != NULL)
 	{
-		PVOID remoteBuffer = VirtualAllocEx(processHandle, NULL, sizeof(dllPath), (MEM_RESERVE | MEM_COMMIT), PAGE_EXECUTE_READWRITE);
+		//allocate memory in target proc
+		PVOID remoteBuffer = VirtualAllocEx(processHandle, NULL, sizeof(dllPath), MEM_COMMIT, PAGE_READWRITE);
 
+		//write path of DLL to newly allocated memory in target proc
 		WriteProcessMemory(processHandle, remoteBuffer, dllPath, sizeof(dllPath), NULL);
 
-		PTHREAD_START_ROUTINE threatStartRoutineAddress = (PTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle(L"Kernel32"), "LoadLibraryW");
+		//Prepare the address of the LoadLibraryW function from Kernel32 in the target proc
+		//The address will be used as the function to execute in the remote thread
+		PTHREAD_START_ROUTINE functionToExecuteAddress = (PTHREAD_START_ROUTINE)GetProcAddress(
+			GetModuleHandle(L"Kernel32"), 
+			"LoadLibraryW"
+		);
 
-		CreateRemoteThread(processHandle, NULL, 0, threatStartRoutineAddress, remoteBuffer, 0, NULL);
+		//Create a remote thread in the target proc and tell it to execute the function with argument remote buffer (dllpath)
+		CreateRemoteThread(processHandle, NULL, 0, functionToExecuteAddress, remoteBuffer, 0, NULL);
 
 		CloseHandle(processHandle);
 
